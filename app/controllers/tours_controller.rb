1bc2df7f4c834a540a6faad2a073f5f0a83362b1
ykbeili <ykbeili@gmail.com>
2022-02-07 23:15:42 -0600

    Move API call to the model

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,32 +1,24 @@
 class ToursController < ApplicationController
-  require 'faraday/net_http'
-  Faraday.default_adapter = :net_http
   include Wicked::Wizard
   steps :step1, :step2, :step3, :step4, :step5, :step6
+  before_action :find_tour, only: [:show]
 
-  def index; end
+  def index
+    random_array = [86_588, 86_697, 86_688]
+    @response = Tour.get_tour(random_array.sample)
+    if @response
+      @tour = Tour.new
+      @tour.agent_name = @response["AgentName"]
+      if @tour.save
+        render :index
+      else
+        render json: { errors: @tour.errors }, status: :unprocessable_entity
+      end
+    end
+  end
 
   def show
-    case step
-    when :step2
-      random_array = [86_588, 86_697, 86_688]
-      @response = Faraday.get("https://www.cotala.com/printjobs/#{random_array.sample}/data.txt")
-      p @response
-      p '@response'
-      @header_elements = []
-      @body_elemets = []
-      array = @response.body.split("\t")
-      array.each_with_index do |e, index|
-        if index < array.count / 2
-          @header_elements.push(e)
-        else
-          @body_elemets.push(e)
-        end
-      end
-      @tour_id = get_tour_id(@header_elements)
-      @images = get_images(@header_elements, @body_elemets)
-      [@header_elements, @body_elemets, @tour_id, @images]
-    end
+    p @tour
     render_wizard
   end
 
@@ -36,22 +28,7 @@ class ToursController < ApplicationController
 
   private
 
-  def get_tour_id(elements)
-    tour_id = ''
-    elements.each_with_index do |element, index|
-      if element.include?('TourID')
-        tour_id = @body_elemets[index]
-        next
-      end
-    end
-    tour_id
-  end
-
-  def get_images(elements, body_elements)
-    images = []
-    elements.each_with_index do |element, index|
-      images.push(body_elements[index]) if element.include?('@mlsPick')
-    end
-    images
+  def find_tour
+    @tour = Tour.find_by_id(params[:tour_id])
   end
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,53 @@
 class Tour < ApplicationRecord
-  serialize :image_name
+  require 'faraday/net_http'
+  Faraday.default_adapter = :net_http
+
+  def self.get_tour(tour_id)
+    begin
+      @response = Faraday.get("https://www.cotala.com/printjobs/#{tour_id}/data.txt")
+      @parsed_response_hash = parse_response(@response.body)
+      @parsed_response_hash
+    rescue StandardError
+      p StandardError
+    end
+  end
+
+  private
+
+  def self.parse_response(response)
+    hash_keys = []
+    hash_values = []
+    parsed_response_hash = {}
+    parsed_response = response.split("\t")
+    parsed_response.each_with_index do |e, index|
+      if index < parsed_response.count / 2
+        hash_keys.push(e)
+      else
+        hash_values.push(e)
+      end
+    end
+    hash_keys.each_with_index do |key, index|
+      parsed_response_hash[key] = hash_values[index]
+    end
+    parsed_response_hash
+  end
+
+  def get_tour_id(elements)
+    tour_id = ''
+    elements.each_with_index do |element, index|
+      if element.include?('TourID')
+        tour_id = @body_elemets[index]
+        next
+      end
+    end
+    tour_id
+  end
+
+  def get_images(keys, values)
+    images = []
+    keys.each_with_index do |key, index|
+      images.push(values[index]) if key.include?('@mlsPick')
+    end
+    images
+  end
 end
