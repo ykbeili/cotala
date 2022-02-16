6402cd2b9fab6391e2c5da4308f52f0c28c3f890
ykbeili <ykbeili@gmail.com>
2022-02-15 22:36:26 -0600

    pass tour id to steps

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,7 +2,7 @@ class Tour < ApplicationRecord
   require 'faraday/net_http'
   Faraday.default_adapter = :net_http
   has_many :images, dependent: :destroy
-
+  # get tour info from cotala
   def self.get_tour(tour_id)
     begin
       @response = Faraday.get("https://www.cotala.com/printjobs/#{tour_id}/data.txt")
@@ -16,6 +16,30 @@ class Tour < ApplicationRecord
   def self.save_record(response)
     @tour = Tour.new
     @tour.agent_name = response["AgentName"]
+    # @tour.agent_phone = response["AgentPhone"]
+    # @tour.agent_email = response["AgentEmail"]
+    # @tour.agent_url = response["AgentUrl"]
+    # @tour.brokerage_name = response["BrokerageName"]
+    # @tour.brokerage_address = response["BrokerageAddress"]
+    # @tour.brokerage_brand = response["BrokerageBrand"]
+    # @tour.broker_logo = response["BrokerLogo"]
+    # @tour.listing_address = response["ListingAddress"]
+    # @tour.show_price = response["ShowPrice"]
+    # @tour.price = response["Price"]
+    # @tour.mls = response["MLS"]
+    # @tour.tax = response["Tax"]
+    # @tour.built = response["Built"]
+    # @tour.no_of_bedrooms = response["Bedrooms"]
+    # @tour.no_of_barhrooms = response["Bathrooms"]
+    # @tour.size = response["Size"]
+    # @tour.lot_or_maint = response["Lot_or_Maint"]
+    # @tour.lot_maint = response["LotMaint"]
+    # @tour.description = response["Description"]
+    # @tour.style = response["Style"]
+    # @tour.type = response["Type"]
+    # @tour.tour_id = response["TourID"]
+    # @tour.num_of_pics = response["NumPics"]
+    # @tour.print_job_id = response["PrintjobID"]
     if @tour.save
       @tour
     else
@@ -24,7 +48,7 @@ class Tour < ApplicationRecord
   end
 
   private
-
+  # parse api response
   def self.parse_response(response)
     parsed_response_hash = {}
     parsed_response = response.split("\t")
@@ -36,7 +60,7 @@ class Tour < ApplicationRecord
     images = get_images(hash_keys, hash_values)
     parsed_response_hash
   end
-
+  # 
   def self.get_response_array(response)
     hash_keys = []
     hash_values = []
