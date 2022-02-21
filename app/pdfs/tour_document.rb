0fb202526e53593be8cae60854cbc2f3f510b1b6
ykbeili <ykbeili@gmail.com>
2022-02-21 14:58:56 -0600

    Fix design for the first page PDF

--- app/pdfs/tour_document.rb
+++ app/pdfs/tour_document.rb
@@ -9,15 +9,23 @@ class TourDocument < Prawn::Document
     broker_image = "https://www.cotala.com/tours/#{@tour.cotala_tour_id}/#{@tour.cotala_tour_id}_#{@tour.images[1].name}"
     borker_log = @tour.broker_logo
     image open(floor_plan), :width => 650, :height => 1600
-    image open(main_image), :width => 550, :height => 800, :at => [650, 1300]
-    image open(borker_log), :width => 200, :height => 200, :at => [1000,1500]
-    image open(borker_log), :width => 200, :height => 200, :at => [1000,1500]
-    image open(broker_image), :width => 200, :height => 200, :at => [650,300]
-    text_box "#{@tour.agent_name}\n Personal Real Estate Corportation", :at => [750, 300]
-    text_box "#{@tour.agent_phone}", :at => [750, 250]
-    text_box "#{@tour.agent_email}", :at => [750, 200]
-    text_box "#{@tour.agent_url}", :at => [750, 150]
-    text_box "#{@tour.listing_address}", :at => [750, 1500]
+    fill_color '6c6d70'
+    font_size 16
+    text_box "Listing Features", :at => [650, 1525], style: "normal"
+    font_size 20
+    text_box "#{@tour.listing_address}", :at => [650, 1500]
+    image open(borker_log), :width => 100, :height => 100, :at => [1050,1550]
+    image open(main_image), :width => 450, :height => 600, :at => [650, 1400]
+    text_box "#{@tour.agent_name}", :at => [760, 300]
+    font_size 8
+    text_box "Personal Real Estate Corportation", :at => [760, 275]
+    font_size 20
+    text_box "#{@tour.agent_phone}", :at => [760, 250]
+    font_size 8
+    text_box "#{@tour.agent_email}", :at => [760, 220]
+    text_box "#{@tour.agent_url}", :at => [760, 200]
+    image open(broker_image), :width => 100, :height => 200, :at => [650,300]
+    image open(borker_log), :width => 200, :height => 100, :at => [950,250]
     start_new_page
     text 'page 2', align: :center, valign: :top
     # bounding_box([100, 300], width: 300, height: 200) do
