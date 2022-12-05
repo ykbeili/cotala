db71f80375601bb575cee3d033ee795c86565dc4
ykbeili <ykbeili@gmail.com>
2022-12-04 18:41:12 -0600

    Change workflow

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -6,8 +6,9 @@ class ToursController < ApplicationController
   include ActionView::Helpers::NumberHelper
   before_action :find_tour, only: %i[show update]
   FTP_LINK = 'ftp.cotala.com'.freeze
-  before_action :get_cotala_tour_id, only: %i[index]
-  def index
+  before_action :get_cotala_tour_id, only: %i[step1]
+
+  def step1
     random_array = [87_416, 87_417, 87_418, 87_419, 87_420, 87_424, 87_425, 87_427]
     if @cotala_tour_id 
       @response = Tour.get_tour(@cotala_tour_id)
@@ -16,7 +17,7 @@ class ToursController < ApplicationController
     end
     @tour = Tour.save_record(@response)
     if @tour.present?
-      render :index
+      render :step1
     else
       render json: { errors: @tour.errors }, status: :unprocessable_entity
     end
@@ -31,40 +32,43 @@ class ToursController < ApplicationController
   end
 
   def create_pdf
+    a = Time.new()
     @tour = Tour.find_by_id(params[:tour_id])
     pdf = TourDocument.new(@tour)
-    # send_data pdf.render,
-    #           filename: "#{@tour.agent_name}-#{@tour.cotala_tour_id}.pdf"
-    ftp = Net::FTP.new(FTP_LINK)
-    ftp.login('tam@cotala.com', 'B*22?Rpdlen+')
-    file_path = "#{Rails.root}/tmp/#{@tour.agent_name}-#{@tour.cotala_tour_id}.pdf"
-    pdf.render_file file_path
-    version = @tour.version || 2
-    ftp.putbinaryfile(file_path, "#{@tour.print_job_id}/#{@tour.print_job_id}-#{version}-b.pdf")
-    redirect_to @tour.hook_url if @tour.hook_url.present?
+    send_data pdf.render,
+              filename: "#{@tour.agent_name}-#{@tour.cotala_tour_id}.pdf"
+    p a - Time.new()
+    p 'difference'
+    # ftp = Net::FTP.new(FTP_LINK)
+    # ftp.login('tam@cotala.com', 'B*22?Rpdlen+')
+    # file_path = "#{Rails.root}/tmp/#{@tour.agent_name}-#{@tour.cotala_tour_id}.pdf"
+    # pdf.render_file file_path
+    # version = @tour.version || 2
+    # ftp.putbinaryfile(file_path, "#{@tour.print_job_id}/#{@tour.print_job_id}-#{version}-b.pdf")
+    # redirect_to @tour.hook_url if @tour.hook_url.present?
   end
 
   def update
     case step
-    when :step2
-      tp = tour_params
-      tp['selected_images'] = tp['selected_images'].split(',')
-      @tour.first_image = tp['selected_images'][0]
-      @tour.second_image = tp['selected_images'][1]
-      @tour.third_image = tp['selected_images'][2]
-      @tour.fourth_image = tp['selected_images'][3]
-      @tour.fifth_image = tp['selected_images'][4]
-      @tour.sixth_image = tp['selected_images'][5]
-      @tour.seventh_image = tp['selected_images'][6]
-      @tour.eighth_image = tp['selected_images'][7]
-      @tour.ninth_image = tp['selected_images'][8]
-      @tour.tenth_image = tp['selected_images'][9]
-      @tour.eleventh_image = tp['selected_images'][10]
-      @tour.twelfth_image = tp['selected_images'][11]
-      @tour.thirteenth_image = tp['selected_images'][12]
-      @tour.fourteenth_image = tp['selected_images'][13]
-      @tour.fifteenth_image = tp['selected_images'][14]
-      @tour.update(tp)
+    # when :step2
+    #   tp = tour_params
+    #   tp['selected_images'] = tp['selected_images'].split(',')
+    #   @tour.first_image = tp['selected_images'][0]
+    #   @tour.second_image = tp['selected_images'][1]
+    #   @tour.third_image = tp['selected_images'][2]
+    #   @tour.fourth_image = tp['selected_images'][3]
+    #   @tour.fifth_image = tp['selected_images'][4]
+    #   @tour.sixth_image = tp['selected_images'][5]
+    #   @tour.seventh_image = tp['selected_images'][6]
+    #   @tour.eighth_image = tp['selected_images'][7]
+    #   @tour.ninth_image = tp['selected_images'][8]
+    #   @tour.tenth_image = tp['selected_images'][9]
+    #   @tour.eleventh_image = tp['selected_images'][10]
+    #   @tour.twelfth_image = tp['selected_images'][11]
+    #   @tour.thirteenth_image = tp['selected_images'][12]
+    #   @tour.fourteenth_image = tp['selected_images'][13]
+    #   @tour.fifteenth_image = tp['selected_images'][14]
+    #   @tour.update(tp)
     when :step3
       tp = tour_params
       @tour.update(tp)
@@ -82,6 +86,8 @@ class ToursController < ApplicationController
   end
 
   def get_cotala_tour_id
+    p params.require(:print_job_id) 
+    p 'params.require(:print_job_id) '
     @cotala_tour_id = params.require(:print_job_id) if params[:print_job_id].present?
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'tours#index'
+  root 'tours#step1'
   match 'download_ofs', to: 'tours#download_ofs', as: 'download', via: :get
   match 'create_pdf', to: 'tours#create_pdf', as: 'create', via: :get
   resources :tours
