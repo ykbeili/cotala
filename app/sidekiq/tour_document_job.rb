9f87d6e59e770e6422585f75931bec03a306a7a8
ykbeili <ykbeili@gmail.com>
2023-03-19 18:47:51 -0600

    Fix a bug on background job

--- app/sidekiq/tour_document_job.rb
+++ app/sidekiq/tour_document_job.rb
@@ -5,13 +5,13 @@ class TourDocumentJob
 
   def perform(id)
     # Do something
-    tour = Tour.find id
-    pdf = TourDocument.new(tour)
+    @tour = Tour.find id
+    @pdf = TourDocument.new(@tour)
     ftp = Net::FTP.new(FTP_LINK)
     ftp.login('tam@cotala.com', 'B*22?Rpdlen+')
-    file_path = "#{Rails.root}/tmp/#{tour.agent_name}-#{tour.cotala_tour_id}.pdf"
-    pdf.render_file file_path
-    version = tour.version || 2
-    ftp.putbinaryfile(file_path, "#{tour.print_job_id}/#{tour.print_job_id}-#{version}-b.pdf")
+    file_path = "#{Rails.root}/tmp/#{@tour.agent_name}-#{@tour.cotala_tour_id}.pdf"
+    @pdf.render_file file_path
+    version = @tour.version || 2
+    ftp.putbinaryfile(file_path, "#{@tour.print_job_id}/#{@tour.print_job_id}-#{version}-b.pdf")
   end
 end
